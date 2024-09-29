a105634b54dd9013d0093e38e5efe4a4a9b7f9b4
Brock lebeau <brocklebeau338gmail.com>
2024-09-28 21:28:06 -0400

    First real commit; confirming api connection

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Searchbar from "./Components/Searchbar";
+import { useEffect, useState } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [query, setQuery] = useState<string>("");
+
+  useEffect(() => {
+    fetch("https://api.openbrewerydb.org/v1/breweries/random")
+      .then((response) => response.json())
+      .then((data) => {
+        console.log("query", data);
+        setQuery(data);
+      });
+  }, []);
+
+  return <div className="App"></div>;
 }
 
 export default App;
---
+++ src/Components/Searchbar.tsx
@@ -0,0 +1,7 @@
+import { Search } from "../Types/search";
+
+const Searchbar = ({ search }: Search) => {
+  return <div></div>;
+};
+
+export default Searchbar;
