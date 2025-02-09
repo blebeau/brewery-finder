6d9b24d78e9063bbf5688f65fd72d1bb2d2d2097
Brock lebeau <brocklebeau338gmail.com>
2025-02-09 08:26:16 -0500

    Tests can run though not finding <p> tag

---
+++ jest.config.js
@@ -0,0 +1,18 @@
+
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  testMatch: ["**/__tests__/**/*.ts?(x)", "**/?(*.)+(test).ts?(x)"],
+  transform: {
+    "^.+\\.(js|ts)$": "ts-jest",
+  },
+  "moduleNameMapper": {
+    "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/__mocks__/fileMock.js",
+    "\\.(scss|sass|css)$": "identity-obj-proxy"
+  },
+  transformIgnorePatterns: [
+    "/node_modules/(?![@autofiy/autofiyable|@autofiy/property]).+\\.js$",
+    "/node_modules/(?![@autofiy/autofiyable|@autofiy/property]).+\\.ts$",
+    "/node_modules/(?![@autofiy/autofiyable|@autofiy/property]).+\\.tsx$",
+  ],
+}
--- src/tests/searchbar.test.tsx
+++ src/tests/searchbar.test.tsx
@@ -2,4 +2,12 @@ import { describe, expect, test } from "@jest/globals";
 import { render, screen, fireEvent } from "@testing-library/react";
 import Searchbar from "../Components/Searchbar";
 
-test("check the search type is on city", () => {});
+test("check proper result displayed when no results found", async () => {
+  render(<Searchbar />);
+
+  await screen.findByRole("p");
+
+  expect(screen.getByRole("p")).toHaveTextContent(
+    "Closing time! No Bars Found"
+  );
+});
